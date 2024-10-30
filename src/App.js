6674a6e0fe3b407c26e39d68e4cc14d50109e0ae
crusoe <crusoe@Ubuntu.myguest.virtualbox.org>
2024-10-30 10:23:08 -0700

    Created Job Board component. Added MUI library. Adjusted some CSS layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,312 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import arrowRight1 from "./arrow-right-1.svg";
+// import coffee1 from "./coffee-1.svg";
 
-function App() {
+// import commentDots1 from "./comment-dots-1.svg"; chat bubble
+
+// group44 = periscope logo
+// import group31 from "./group-31.png"; left arrow gray
+
+// import image from "./image.png";
+// import image1 from "./image.svg";
+//company carousel
+// import maskGroup2 from "./mask-group-2.png"; 
+// import maskGroup3 from "./mask-group-3.png";
+
+// import maskGroup4 from "./mask-group-4.png"; therapist headshot
+
+
+// import maskGroup from "./public/portrait-of-massage-therapist-massaging-neck-of-yo-6KXZU98 1.png";
+// import microsoftteamsImage71 from "./MicrosoftTeams-image (7).png";
+
+//**start rating image for carousel */
+// import star1 from "./star-1.svg";
+// import star2 from "./star-2.svg";
+// import star3 from "./star-3.svg";
+// import star4 from "./star-4.svg";
+// import star5 from "./star-5.svg";
+import "./App.css";
+// import vector from "./airplane.svg";
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="servio">
+      <div className="div">
+        <div className="overlap">
+          <img
+            className="mask-group"
+            alt="Mask group"
+            src="https://c.animaapp.com/PMiAzVHf/img/mask-group@2x.png"
+          />
+
+          <div className="group">
+            <div className="rectangle" />
+          </div>
+
+          <img
+            className="img"
+            alt="Mask group"
+            src="https://c.animaapp.com/PMiAzVHf/img/mask-group-1.png"
+          />
+        </div>
+
+        <div className="overlap-group">
+          <div className="overlap-2">
+            <p className="text-wrapper">
+              What our platform can do for your needs.
+            </p>
+
+            <p className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+              mollis nisi id lectus hendrerit pulvinar.&nbsp;&nbsp;Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Duis mollis nisi id
+              lectus hendrerit pulvinar.
+            </p>
+
+            <div className="overlap-3">
+              <div className="group-2">
+                <div className="text-wrapper-2">Home</div>
+
+                <div className="text-wrapper-3">About</div>
+
+                <div className="text-wrapper-4">Services</div>
+
+                <div className="text-wrapper-5">Contact</div>
+              </div>
+
+              <div className="navbar">
+                <div className="text-wrapper-6">fb</div>
+
+                <div className="text-wrapper-7">ig</div>
+
+                <div className="text-wrapper-8">yt</div>
+
+                <div className="text-wrapper-9">lk</div>
+              </div>
+
+              <div className="group-3">
+                <div className="overlap-4">
+                  <div className="group-4" />
+
+                  <div className="rectangle-2" />
+                </div>
+
+                <div className="group-5" />
+              </div>
+
+              <img
+                className="microsoftteams-image"
+                alt="Microsoftteams image"
+                src="https://c.animaapp.com/PMiAzVHf/img/microsoftteams-image--7--1.png"
+              />
+            </div>
+          </div>
+
+          <div className="overlap-group-wrapper">
+            <div className="div-wrapper">
+              <p className="p">Get in touch with us</p>
+            </div>
+          </div>
+
+          <img
+            className="group-6"
+            alt="Group"
+            src="https://c.animaapp.com/PMiAzVHf/img/group-44.png"
+          />
+        </div>
+
+        <div className="overlap-wrapper">
+          <div className="div-wrapper">
+            <p className="p">Get in touch with us</p>
+          </div>
+        </div>
+
+        <p className="text-wrapper-10">We work with over 100 companies.</p>
+
+        <p className="text-wrapper-11">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis
+          nisi id lectus hendrerit pulvinar. Nam elementum auctor facilisis.
+          Quisque ultrices in purus nec fermentum. Phasellus nec lacinia orci,
+          ut consectetur lorem
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <img
+          className="mask-group-2"
+          alt="Mask group"
+          src="https://c.animaapp.com/PMiAzVHf/img/mask-group-2.png"
+        />
+
+        <img
+          className="mask-group-3"
+          alt="Mask group"
+          src="https://c.animaapp.com/PMiAzVHf/img/mask-group-3.png"
+        />
+
+        <div className="group-7">
+          <div className="overlap-5">
+            <div className="rectangle-3" />
+
+            <div className="group-8">
+              <div className="overlap-6">
+                <div className="text-wrapper-12">Very satisfied.</div>
+
+                <p className="text-wrapper-13">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  mollis nisi id lectus hendrerit pulvinar. Nam elementum auctor
+                  facilisis. Quisque ultrices in purus nec fermentum. Phasellus
+                  nec lacinia orci, ut consectetur lorem.
+                </p>
+              </div>
+
+              <div className="group-9">
+                <div className="overlap-group-2">
+                  <div className="rectangle-4" />
+
+                  <img
+                    className="group-9"
+                    alt="Mask group"
+                    src="https://c.animaapp.com/PMiAzVHf/img/mask-group-4@2x.png"
+                  />
+                </div>
+              </div>
+
+              <div className="group-10">
+                <img
+                  className="star"
+                  alt="Star"
+                  src="https://c.animaapp.com/PMiAzVHf/img/star-1.svg"
+                />
+
+                <img
+                  className="star-2"
+                  alt="Star"
+                  src="https://c.animaapp.com/PMiAzVHf/img/star-2.svg"
+                />
+
+                <img
+                  className="star-3"
+                  alt="Star"
+                  src="https://c.animaapp.com/PMiAzVHf/img/star-3.svg"
+                />
+
+                <img
+                  className="star-4"
+                  alt="Star"
+                  src="https://c.animaapp.com/PMiAzVHf/img/star-4.svg"
+                />
+
+                <img
+                  className="star-5"
+                  alt="Star"
+                  src="https://c.animaapp.com/PMiAzVHf/img/star-5.svg"
+                />
+              </div>
+
+              <div className="group-11">
+                <div className="arrow-right-wrapper">
+                  <div className="arrow-right">
+                    <img
+                      className="vector"
+                      alt="Vector"
+                      src="https://c.animaapp.com/PMiAzVHf/img/vector.png"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="text-wrapper-14">1/6</div>
+          </div>
+        </div>
+
+        <img
+          className="group-12"
+          alt="Group"
+          src="https://c.animaapp.com/PMiAzVHf/img/group-31@2x.png"
+        />
+
+
+        <p className="text-wrapper-23">
+          If your costs are going up and budgets squeezed, we can help.
+        </p>
+
+        <div className="group-20">
+          <div className="group-21">
+            <div className="overlap-group-4">
+              <p className="text-wrapper-24">
+                We are Periscope and we are going to help you.
+              </p>
+
+              <div className="text-wrapper-25">In-home assessments</div>
+            </div>
+
+            <div className="vector-wrapper">
+              <img
+                className="vector-2"
+                alt="Vector"
+                src="https://c.animaapp.com/PMiAzVHf/img/vector-1.svg"
+              />
+            </div>
+          </div>
+
+          <div className="overlap-9">
+            <p className="text-wrapper-24">
+              We are Periscope and we are going to help you.
+            </p>
+
+            <div className="text-wrapper-25">Chart reviews</div>
+          </div>
+
+          <div className="overlap-10">
+            <p className="text-wrapper-24">
+              We&#39;re here when you need us 24/7/365.
+            </p>
+
+            <div className="text-wrapper-25">27/7 support</div>
+          </div>
+
+          <div className="overlap-11">
+            <div className="rectangle-6" />
+
+            <img
+              className="coffee"
+              alt="Coffee"
+              src="https://c.animaapp.com/PMiAzVHf/img/coffee-1.svg"
+            />
+          </div>
+
+          <div className="overlap-12">
+            <div className="rectangle-7" />
+
+            <img
+              className="comment-dots"
+              alt="Comment dots"
+              src="https://c.animaapp.com/PMiAzVHf/img/comment-dots-1.svg"
+            />
+          </div>
+
+          <div className="text-wrapper-26">know more</div>
+
+          <div className="text-wrapper-27">know more</div>
+
+          <div className="text-wrapper-28">know more</div>
+        </div>
+
+        <p className="text-wrapper-29">
+          Periscope is going to help you find what you need.
+        </p>
+
+        <p className="text-wrapper-30">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis
+          nisi id lectus hendrerit pulvinar. Nam elementum auctor facilisis.
+          Quisque ultrices in purus nec fermentum. Phasellus nec lacinia orci,
+          ut consectetur lorem. Pellentesque eu varius arcu, facilisis
+          sollicitudin augue. In tincidunt sapien at posuere auctor.
+        </p>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/JobBoard.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Grid2, Card, CardContent, Typography, Button, TextField, MenuItem, Select, Pagination } from '@mui/material';
+
+const JobBoard = () => {
+  const [search, setSearch] = useState('');
+  const [location, setLocation] = useState('');
+  const [department, setDepartment] = useState('');
+  const [page, setPage] = useState(1);
+
+  const jobs = [
+    { id: 1, title: 'Software Engineer', department: 'Engineering', location: 'New York' },
+    { id: 2, title: 'Product Manager', department: 'Product', location: 'San Francisco' },
+    // ...other job entries
+  ];
+
+  const handleSearchChange = (e) => setSearch(e.target.value);
+  const handleLocationChange = (e) => setLocation(e.target.value);
+  const handleDepartmentChange = (e) => setDepartment(e.target.value);
+  const handlePageChange = (e, value) => setPage(value);
+
+  return (
+    <div style={{ padding: '20px' }}>
+      {/* Search and Filter Controls */}
+      <Grid2 container spacing={2} style={{ marginBottom: '20px' }}>
+        <Grid2 item xs={12} md={4}>
+          <TextField
+            label="Search Titles"
+            variant="outlined"
+            fullWidth
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </Grid2>
+        <Grid2 item xs={6} md={4}>
+          <Select
+            value={location}
+            onChange={handleLocationChange}
+            displayEmpty
+            fullWidth
+          >
+            <MenuItem value=""><em>All Locations</em></MenuItem>
+            <MenuItem value="New York">New York</MenuItem>
+            <MenuItem value="San Francisco">San Francisco</MenuItem>
+          </Select>
+        </Grid2>
+        <Grid2 item xs={6} md={4}>
+          <Select
+            value={department}
+            onChange={handleDepartmentChange}
+            displayEmpty
+            fullWidth
+          >
+            <MenuItem value=""><em>All Departments</em></MenuItem>
+            <MenuItem value="Engineering">Engineering</MenuItem>
+            <MenuItem value="Product">Product</MenuItem>
+          </Select>
+        </Grid2>
+      </Grid2>
+
+      {/* Job Cards Grid */}
+      <Grid2 container spacing={3}>
+        {jobs.map((job) => (
+          <Grid2 item xs={12} sm={6} md={4} key={job.id}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">{job.title}</Typography>
+                <Typography color="textSecondary">{job.department}</Typography>
+                <Typography color="textSecondary">{job.location}</Typography>
+                <Button variant="contained" color="primary" style={{ marginTop: '10px' }}>Apply</Button>
+              </CardContent>
+            </Card>
+          </Grid2>
+        ))}
+      </Grid2>
+
+      {/* Pagination */}
+      <Pagination
+        count={10}  // This would dynamically change based on data
+        page={page}
+        onChange={handlePageChange}
+        color="primary"
+        style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}
+      />
+    </div>
+  );
+};
+
+export default JobBoard;
